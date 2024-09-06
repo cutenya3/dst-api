e8e9cdabcbf5fda5eb2c06cc851233e446ddb573
i0nya <150973088+cutenya3@users.noreply.github.com>
2024-09-06 10:27:14 +0800

    Update aoespell.lua
    111，点这个就能提交了吗，Ou<

--- scripts/components/aoespell.lua
+++ scripts/components/aoespell.lua
@@ -3,42 +3,43 @@ data = {
     {
         method = "SetSpellFn",
         params = {
-            {param = "fn", explain = "", type = ""},
+            {param = "fn", explain = "判断函数", type = "function"},
 
         },
         returns = {
             
         },
-        tips = "",
-        author = "",
+        tips = "设置施法判断函数",
+        author = "i0",
     },
         
     {
         method = "CastSpell",
         params = {
-            {param = "doer", explain = "", type = ""},
-            {param = "pos", explain = "", type = ""},
+            {param = "doer", explain = "施法者", type = "实体"},
+            {param = "pos", explain = "位置", type = "Vector3"},
 
         },
         returns = {
-            
+            {explain = "是否成功", type = "bool"},
+            {explain = "失败原因", type = "string"},
         },
-        tips = "",
-        author = "",
+        tips = "在目标位置施法",
+        author = "i0",
     },
         
     {
         method = "CanCast",
         params = {
-            {param = "doer", explain = "", type = ""},
-            {param = "pos", explain = "", type = ""},
+            {param = "doer", explain = "施法者", type = "实体"},
+            {param = "pos", explain = "位置", type = "Vector3"},
 
         },
         returns = {
-            
+            {explain = "是否允许", type = "bool"}
         },
-        tips = "",
-        author = "",
+        tips = "判断目标位置可否施法",
+        author = "i0",
     },
         
 }
